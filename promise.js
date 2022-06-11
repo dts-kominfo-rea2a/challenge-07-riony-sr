c900802c5d70faa0e88330d5a999582438db7760
riony-sr <riony.sr@gmail.com>
2022-06-12 05:11:04 +0700

    Latihan Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const data1 = await promiseTheaterIXX();
+    const data2 = await promiseTheaterVGC();
+    const arrFilm = (await data1).concat(await data2);
+    return arrFilm.filter((Element) => Element.hasil === emosi).length;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
